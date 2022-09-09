5aa16a1344bfcfffdbe88ed8da350a0dfe256d38
rizalkhisyam <162410101048@cs.unej.ac.id>
2022-09-09 15:07:12 +0700

    done slide

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,15 @@ module.exports = {
         primary: '#0A263D',
         secondary: '#F2F4F7',
         grey: '#8B95A5',
-        link: '#4B61DD'
+        link: '#4B61DD',
+        button: '#F7601A',
+        screen: '#21228C'
       },
       height: {
         '400': '25rem',
         '300': '18rem',
-        '152': '9.5rem'
+        '152': '9.5rem',
+        '812': '50.75rem'
       },
       width: {
         '152': '9.5rem'
